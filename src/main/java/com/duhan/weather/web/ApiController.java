f45a5e0af72cd504a7d93de67996148014a1a388
DuhanMo <devduhanmo@gmail.com>
2021-04-19 16:22:47 +0900

    도시이름 드랍다운 완성

--- src/main/java/com/duhan/weather/web/ApiController.java
+++ src/main/java/com/duhan/weather/web/ApiController.java
@@ -1,44 +1,34 @@
 package com.duhan.weather.web;
 
+import com.duhan.weather.domain.Weather;
+import com.duhan.weather.domain.WeatherRepository;
 import com.duhan.weather.service.WeatherService;
 import lombok.RequiredArgsConstructor;
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.http.HttpSession;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 public class ApiController {
     private final WeatherService weatherService;
+    private final WeatherRepository weatherRepository;
 
 
-    @GetMapping("/save")
+    @GetMapping("/api/save")
     public Long saveInitData() {
         return weatherService.saveInitData();
     }
+    @GetMapping("/api/all")
+    public List<Weather> getAll() {
+        return weatherRepository.findAll();
+    }
 
-    @GetMapping("/delete")
+    @GetMapping("/api/delete")
     public String deleteBeforeData() {
         return weatherService.deleteBeforeData();
     }
 
-//    @GetMapping("/init")
-//    public String initialize() {
-//         weatherService.initialize();
-//        return "index";
-//    }
 
 }
